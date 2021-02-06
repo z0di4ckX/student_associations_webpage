6ada3ee1294421e97066ee2435db31274486b359
Marcos <50970242+ketchupstrains@users.noreply.github.com>
2021-02-06 12:39:54 -0400

    Create dump.php

---
+++ src/dump.php
@@ -0,0 +1,60 @@
+<!-- End the section -->
+<main>
+    <!-- This is the page title  -->
+    <div class="pagetitle">
+      <h1>Asociaciones Estudiantiles</h1>
+    </div>
+    <!-- End of Page title   -->
+    <!-- Card table begins  -->
+    <table class="cardtable" with=100%>
+      <tr>
+        <td>
+          <div class="card">
+            <a href="icsa.php"><img src="https://scontent.fsig3-1.fna.fbcdn.net/v/t1.0-9/119110816_1023516111420939_1634671659710261333_n.png?_nc_cat=105&ccb=2&_nc_sid=09cbfe&_nc_ohc=XFwaiF1BPWIAX85gm3o&_nc_ht=scontent.fsig3-1.fna&oh=cadb4179fc253736545d658646d4dffe&oe=60430FB2" alt="Avatar" width="250" height="250"></a>
+            <div class="containerl">
+              <h4><b>ICSA</b></h4>
+              <p>Ciencias y Tecnologia</p>
+            </div>
+        </td>
+        <td>
+          <div class="card">
+            <a href="ieee.html"><img src="images/IEEE.jpg" alt="Avatar" width="250" height="250"></a>
+            <div class="containerl">
+              <h4><b>IEEE</b></h4>
+              <p>Ciencias y Tecnologia</p>
+        </td>
+        <td>
+          <div class="card">
+            <a href="icsa.html"><img src="images/Inter Designers.jpg" alt="Avatar" width="250" height="250"></a>
+            <div class="containerl">
+              <h4><b>Inter Designers</b></h4>
+              <p>Estudios humanisticos</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="card">
+            <a href="aecpf.html"><img src="images/Logo de asociacion.png" alt="Avatar" width="250" height="250"></a>
+            <div class="containerl">
+              <h4><b>AECPF</b></h4>
+              <p>Estudios graduados</p>
+            </div>
+        </td>
+        <td>
+          <div class="card">
+            <a href="SECAT.html"><img src="images/SECAT.jpg" alt="Avatar" width="250" height="250"></a>
+            <div class="containerl">
+              <h4><b>SECAT</b></h4>
+              <p>Ciencias Ambientales</p>
+        </td>
+        <td>
+          <div class="card">
+            <a href="icsa.html"><img src="images/MEDLIFE.jpg" alt="Avatar" width="250" height="250"></a>
+            <div class="containerl">
+              <h4><b>MEDLIFE</b></h4>
+              <p>Ciencias de la salud</p>
+        </td>
+      </tr>
+    </table>
+    <!-- End Of Card table  -->
+  </main>
